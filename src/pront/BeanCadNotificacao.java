72e7c14970cd888576996381433cba0bfd0f8b03
Luís C. <luiscv86@gmail.com>
2013-08-13 00:41:45 -0300

    Create BeanCadNotificacao.java

---
+++ src/pront/BeanCadNotificacao.java
@@ -0,0 +1,62 @@
+/*investigador
+ * data notificacao
+ * unidade notificadora
+ * unidade federal
+ * municipio
+ * modo de detecção
+ * modo de entrada
+*/
+package pront;
+
+/**
+ *
+ * @author Luis
+ */
+public class BeanCadNotificacao {
+    protected String investivador ="", datanot ="",unidadenot="";
+    protected byte uf=0,municipio=0,mdetec=0,mentrada=0;
+/*
+    void setInv(String inv){
+        this.inv = inv;
+    }
+    String getInv(){
+        return inv;
+    }
+    void setDatanot(String datanot){
+        this.datanot = datanot;
+    }
+    String getDatanot(){
+        return datanot;
+    }
+    void setUninot(String uninot){
+        this.uninot = uninot;
+    }
+    String getUninot(){
+        return uninot;
+    }
+    void setUf(int uf){
+        this.uf = uf;
+    }
+    int getUf(){
+        return uf;
+    }
+    void setMunicipio(int municipio){
+        this.municipio = municipio;
+    }
+    int getMunicipio(){
+        return municipio;
+    }
+    void setMdetec(int mdetec){
+        this.mdetec = mdetec;
+    }
+    int getMdetec(){
+        return mdetec;
+    }
+    void setMentrada(int mentrada){
+        this.mentrada = mentrada;
+    }
+    int getMentrada(){
+        return mentrada;
+    }
+*/
+}
