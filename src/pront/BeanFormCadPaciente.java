7e2ccb912c3dfba53c8971ddd660feaae21319ee
Luís C. <luiscv86@gmail.com>
2013-08-13 00:45:31 -0300

    Create BeanFormCadPaciente.java

---
+++ src/pront/BeanFormCadPaciente.java
@@ -0,0 +1,94 @@
+package pront;
+
+/**
+ *
+ * @author ead
+ */
+ class BeanFormCadPaciente{
+    //Set Get da etapa 1
+
+    protected String nome="", data="0", idade="0", ocupacao="", telefone="0", sus="0", mae="";
+    protected byte genero=0, etnia=0, escolaridade=0;
+    /*
+    void setNome(String nome){
+        this.nome = nome;
+    }
+    String getNome(){
+        return this.nome;
+    }
+    void setData(String data){
+        this.data = data;
+    }
+    String getData(){
+        return this.data;
+    }
+    void setIdade(String idade){
+        this.idade = idade;
+    }
+    String getIdade(){
+        return this.idade;
+    }
+    void setOcupacao(String ocupacao){
+        this.ocupacao = ocupacao;
+    }
+    String getOcupacao(){
+        return this.ocupacao;
+    }
+    void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    String getTelefone(){
+        return this.telefone;
+    }
+    void setSus(String sus){
+        this.sus = sus;
+    }
+    String getSus(){
+        return this.sus;
+    }
+    void setMae(String mae){
+        this.mae = mae;
+    }
+    String getMae(){
+        return this.mae;
+    }
+    void setGenero(byte genero){
+        this.genero = genero;
+    }
+    byte getGenero(){
+        return this.genero;
+    }
+    void setEtnia(byte etnia){
+        this.etnia = etnia;
+    }
+    byte getEtnia(){
+        return this.etnia;
+    }
+    void setEscolaridade(byte escolaridade){
+        this.escolaridade = escolaridade;
+    }
+    byte getEscolaridade(){
+        return this.escolaridade;
+    }
+    */
+    /*void setGenero(String genero){
+        this.genero = genero;
+    }
+    String getGenero(){
+        return this.genero;
+    }
+
+    void setEtnia(String etnia){
+        this.etnia = etnia;
+    }
+    String getEtnia(){
+        return this.etnia;
+    }
+
+    void setEscolaridade(String escolaridade){
+        this.escolaridade = escolaridade;
+    }
+    String getEscolaridade(){
+        return this.escolaridade;
+    }*/
+}
