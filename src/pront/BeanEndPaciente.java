e3084cf91e09ebfcac44364e7111557e997c4999
Luís C. <luiscv86@gmail.com>
2013-08-13 00:45:07 -0300

    Create BeanEndPaciente.java

---
+++ src/pront/BeanEndPaciente.java
@@ -0,0 +1,19 @@
+package pront;
+
+/**
+ *
+ * @author Luis
+ */
+public class BeanEndPaciente {
+    protected String codnot="", endereco="", numend="0", complemento="",referencia="",cep="0",bairro="";
+    protected byte zona=0, municipio=0;
+    /*TextField campo30 = new TextField("Codigo da Notificação:", "", 5, TextField.ANY);
+    TextField campo31 = new TextField("Endereço", "", 40, TextField.ANY);
+    TextField campo32 = new TextField("Numero do Endereço", "", 8, TextField.NUMERIC);
+    TextField campo33 = new TextField("Complemento", "", 20, TextField.ANY);
+    TextField campo34 = new TextField("Ponto de Referencia", "", 40, TextField.ANY);
+    TextField campo35 = new TextField("CEP", "", 8, TextField.NUMERIC);
+    TextField campo36 = new TextField("Bairro:", "", 15, TextField.ANY);
+    final String [] zona = {"Ignorado", "Rural", "Urbana", "Rural/Urbana"};
+    final String [] municipio = {"Ignorado", "Curitiba", "Colombo", "Almirante Tamandare", "Pinhais"};*/
+}
