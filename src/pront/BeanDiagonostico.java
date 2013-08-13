ba5b4a4c552e30ab3dfa786b327018eed3b17321
Luís C. <luiscv86@gmail.com>
2013-08-13 00:44:37 -0300

    Create BeanDiagonostico.java

---
+++ src/pront/BeanDiagonostico.java
@@ -0,0 +1,11 @@
+package pront;
+
+/**
+ *
+ * @author Luis
+ */
+public class BeanDiagonostico {
+    //protected int codnotificacao;
+    protected String numlesoes="0",numtroncos="0",numcontatos="0",datadiagonostico="0",observacoes="";
+    protected int incapacidade=0, baciloscopia=0,formaclinica=0,classoperacional=0,doencarelacionada=0,cid10=0;
+}
