c8465a146ee139af11588537dd6e6f0f8ed38cbb
Luís C. <luiscv86@gmail.com>
2013-08-13 00:45:54 -0300

    Create BeanTratamento.java

---
+++ src/pront/BeanTratamento.java
@@ -0,0 +1,11 @@
+package pront;
+
+/**
+ *
+ * @author Luis
+ */
+public class BeanTratamento {
+    protected String datatratamento="0";
+    protected int esquematerapeutico=0;
+    protected int situacao=0;
+}
